d6f2476bba82ff0f0b63a36e1719609ed3dd0634
jaymehta6770 <jaym34435@gmail.com>
2026-02-25 15:21:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -222,7 +222,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # 📺 FULL SEASON ONE QUALITY
 # example: title_s01_720p
 # =====================================================
-season_quality = re.match(r"(.+)_s(\d+)_(\d+p)", query)
+try:
+    season_quality = re.match(r"(.+)_S(\d+)_(\d+p)", query)
+except Exception as e:
+    print(e)
 
 if season_quality:
     title, s_num, quality = season_quality.groups()
